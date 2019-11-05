88039bca6de05e3a6a20e65bef91bfc140283064
SukhadaKhatu <khatu.s@husky.neu.edu>
2019-11-05 04:32:34 +0000

    packag

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,10 +16,10 @@ alias TimesheetDemo.Tasks.Task
 alias TimesheetDemo.Requests.Request
 
 password = Argon2.add_hash("password")[:password_hash]
-# Repo.insert!(%User{name: "Alice", email: "alice.example.com", password_hash: password})
-# Repo.insert!(%User{name: "Bob", email: "bob.example.com", password_hash: password, manager: 1})
-# Repo.insert!(%User{name: "Faith", email: "faith.example.com", password_hash: password, manager: 1})
-Repo.insert!(%Task{job_code: "JOB2", time: 1, desc: "Completed task 2", user_id: 2, request_id: 1})
+Repo.insert!(%User{name: "Alice", email: "alice.example.com", password_hash: password})
+Repo.insert!(%User{name: "Bob", email: "bob.example.com", password_hash: password, manager: 1})
+Repo.insert!(%User{name: "Faith", email: "faith.example.com", password_hash: password, manager: 1})
+#Repo.insert!(%Task{job_code: "JOB2", time: 1, desc: "Completed task 2", user_id: 2, request_id: 1})
 # date = Ecto.Date.cast("2019-01-01")
 # birthday = "01/01/2019"
 
